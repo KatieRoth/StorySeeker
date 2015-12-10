e233f5af24b2d42c1ea6042901a57f6a54717aab
maxitaxi_kindred <maxwell.kindred@drake.edu>
2015-12-10 06:43:37 -0600

    fixed selected books list, it still crashes the first time you click it, but every subsequent click it works. Dont know what the issue is. My wishlist isnt working for some reason. Added an "add to wishlist" button to the bookDescription page. When clicked it calls an addToWishList method that adds relevant information to a global book object that is then recieved by the wishlist activity and then added into the array list. Working on getting the parcelable to actually transfer information right. 90% sure itll be ready before Urness downloads the code for the user test. I am also finishing the database version, its a lot simpler and easier to manage code.

---
+++ app/src/main/java/com/example/katie/storyseeker/GlobalBook.java
@@ -0,0 +1,63 @@
+package com.example.katie.storyseeker;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.provider.Settings;
+
+/**
+ * Created by maxitaxi_kindred on 12/10/15.
+ */
+public class GlobalBook  implements Parcelable{
+    public String title;
+    public String author;
+    public String bookCover;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void setBookCover(String bookCover){
+        this.bookCover = bookCover;
+    }
+
+    public String getTitle(String title){
+        return title;
+    }
+
+    public String getAuthor(String author){
+        return author;
+    }
+
+    public String getBookCover(String bookCover){
+        return bookCover;
+    }
+
+    public void writeToParcel(Parcel parcel, int flags){
+        parcel.writeString(title);
+        parcel.writeString(author);
+        parcel.writeString(bookCover);
+    }
+
+    public int describeContents(){
+        return 0;
+    }
+    public static Creator<GlobalBook> CREATOR = new Creator<GlobalBook>() {
+        @Override
+        public GlobalBook createFromParcel(Parcel source) {
+
+            GlobalBook gbook = new GlobalBook();
+            gbook.title = source.readString();
+            gbook.author = source.readString();
+            gbook.bookCover = source.readString();
+            return gbook;
+        }
+        @Override
+        public GlobalBook[] newArray(int size) {
+            return new GlobalBook[size];
+        }
+    };
+}
